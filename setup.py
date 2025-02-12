fc6e51b0272cf41b15df41e631d35f870af41c5f
akshats1 <akshats063@gmail.com>
2025-02-12 11:05:32 +0530

    code added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="image_visualize",
+    version="0.0.1",
+    author="Akshat",
+    author_email="akshats063@gmail.com",
+    packages=find_packages()
+)
